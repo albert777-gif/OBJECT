ce2691ff28b79fe2e70788ab4913b285c8552c71
soul-mortal <yodgorsalohiddinov44@gmail.com>
2025-03-24 16:58:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+ let num = 'mustafo';
+alert(`hello ${num}`);
+
+ let num2 = 5;
+ let num3 = 10;
+ alert(`${num2} + ${num3} = ${num2 + num3}`)
+
+for(let i = 0; i <= 100; i++) {
+    if(i % 2 !== 0) {
+        console.log(i)
+    }
+}
+alert(`${num3} - ${num2} = ${num3 - num2}`)
+
+alert(`${num3} / ${num2} = ${num3 / num2}`)
+
+
+
+alert(`${num3} * ${num2} = ${num3 * num2}`)
+
+
+
+alert(`hello ${num}
+${num2} + ${num3} = ${num2 + num3}
+${num3} - ${num2} = ${num3 - num2}
+${num3} / ${num2} = ${num3 / num2}
+${num3} * ${num2} = ${num3 * num2}`);
